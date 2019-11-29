a9810d4a004803f994cd0f7abe72abc080b95858
aydin.can <aydin.can@beyn.com.tr>
2019-11-29 17:36:09 +0300

    Commentler düzeltildi.

--- src/app/counter.reducer.ts
+++ src/app/counter.reducer.ts
@@ -3,12 +3,9 @@ import { increment, decrement, reset } from './ counter.actions';  // src/app/ c
  
 export const initialState = 0; // Bu zorunlu  initialState tanımlanmak zorunda.  Aşağıdaki _counterReducer  initial state olarak bu değeri arayacak.
  
-const _counterReducer = createReducer(initialState,
-  on(increment, state => state + 1),  // on metodu yeni versiyonla geldi bu temelde switch-case yapısıyla aynı... bu actiona tıklandığında initialState'e 1 eklenecek ve güncel state return edilecek.
-  on(decrement, state => state - 1),  // 7 satırla aynı state'i 1 eksiltip return eder
-  on(reset, state => 0), // state'ı 0 yapıp return eder.
+export const counterReducer = createReducer(initialState, 
+  on(increment, state => state + 1),  // on metodu yeni versiyonla geldi bu temelde switch-case yapısıyla aynı... bu aksyon tetiklendiğinde State'e 1 eklenecek ve güncel state return edilecek.
+  on(decrement, state => state - 1),  //  satırla aynı state'i 1 eksiltip return eder
+  on(reset, () => 0), // state'ı 0 yapıp return eder.
 ); 
  
-export function counterReducer(state, action) { // Bu _counterReducer reducer'ını export etmek için kullandığımız bir fonskyon
-  return _counterReducer(state, action);
-}
--- src/app/my-counter/my-counter.component.ts
+++ src/app/my-counter/my-counter.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Store, select } from '@ngrx/store'; // Store ve select  kullanıldığı için import edild
+import { Store, select } from '@ngrx/store'; // Store ve select  kullanıldığı için import edildi
 import { Observable } from 'rxjs'; // Observable metodlarını kullandığımız için  bunu import ettik //  https://rxjs-dev.firebaseapp.com/guide/operators
 import { increment, decrement, reset } from '../ counter.actions'; // src/app/ counter.actions.ts dosyasında  createAction metoduyla oluşturduğumuz Actionlar
  
@@ -9,20 +9,20 @@ import { increment, decrement, reset } from '../ counter.actions'; // src/app/ c
   styleUrls: ['./my-counter.component.css'],
 })
 export class MyCounterComponent {
-  count$: Observable<number>; // counter$  number türünde bir observable demek
+  count$: Observable<number>; // count$  number türünde bir observable demek
  
-  constructor(private store: Store<{ count: number }>) {  // Dependency injection yöntemiyle Store içinde number türünde count değişkeni oluşturuldu
+  constructor(private store: Store<{ count: number }>) {  // Statin tipi belirtildi. bir obje olduğu ve içerisinde number türünde count değişkeni olduğu söylendi.
     this.count$ = store.pipe(select('count')); //  select metodu burada Store içindeki count değişkenini seçiyor.  bu observable  count$ değişkenine atanıyor.
   }
   increment() {   // src/app/my-counter/my-counter.component.html içinde  (click) ile çağırdığımız metod
-    this.store.dispatch(increment());
+    this.store.dispatch(increment()); // increment aksyonu payload göndermeden tetikledik. src/app/counter.reducer.ts altındaki _counterReducer'na göndermiş olduk
   }
  
   decrement() { // src/app/my-counter/my-counter.component.html içinde  (click) ile çağırdığımız metod
-    this.store.dispatch(decrement());
+    this.store.dispatch(decrement()); // decrement aksyonu payload göndermeden tetikledik. src/app/counter.reducer.ts altındaki _counterReducer'na göndermiş olduk
   }
  
   reset() { // src/app/my-counter/my-counter.component.html içinde  (click) ile çağırdığımız metod
-    this.store.dispatch(reset());
+    this.store.dispatch(reset()); // reset aksyonu payload göndermeden tetikledik. src/app/counter.reducer.ts altındaki _counterReducer'na göndermiş olduk
   }
 }
